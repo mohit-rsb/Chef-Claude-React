4ddfb2aa1834c32ecbcad261b4704cdbd15861e6
mohit-rsb <mohit.bhardwajjj19@gmail.com>
2025-03-11 22:57:22 +0530

    added ui for get-recipe

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default function Main(){
-    const [ingredients, setIngredients] = React.useState(["Chicken", "Oregano", "Tomatoes"])
+    const [ingredients, setIngredients] = React.useState([])
 
     const ingredientsListItems =  ingredients.map((ingredient) =>
             <li key={ingredient}> {ingredient}</li>
@@ -39,9 +39,19 @@ export default function Main(){
                     name = "new-ingredient" />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+            {ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul>
+                    {ingredientsListItems}
+                </ul>
+                {ingredients.length > 3 && <div className="get-recipe">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>}
+            </section>}   
         </main>
     )
 }
